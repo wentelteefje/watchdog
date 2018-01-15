bf8c038614f6a5508a4cdfc99c302c61f5003a00
Kevin <kevin@a5xz2.com>
2018-01-15 01:21:49 +0000

    Added additional sleep timers

--- watchdog.sh
+++ watchdog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 while true
 do
-  if ! ping -c 1 -w 30 192.168.178.20 >/dev/null
+  if ! ping -c 1 -w 60 192.168.178.20 >/dev/null
   then
     date | tr '\n' ' ' >> watchdog.log
     echo "Keine Response vom Rig. Leite Neustart ein." >> watchdog.log
     python pwr_off.py
+    sleep 600;
   else
     echo "Alles OK."
   fi
